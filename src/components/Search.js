44624865e5cba6a7f32a543f8bd20db66b46e1a8
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-10-27 14:55:45 -0600

    intelligent search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react'
 import { getStrainOfStrains } from "../helpers";
+import axios from 'axios';
 
 class Search extends Component {
- state = {
-   query: '',
- }
 
+    constructor(props){
+        super(props)
+        this.state = {
+            query: '',
+            result: []
+        }
+      }
+
+//       componentDidMount(){
+//         //console.log('Query:', query);
+//   axios.get(`http://strainapi.evanbusse.com/yh2fbKy/strains/search/name/${this.state.query}`)
+//         .then(json => json.data.results.map(result => (
+//           {
+//             name: `${result.name}`,
+//             race: `${result.race}`,
+//             desc: `${result.desc}`,
+//           })))
+//         .then(newData => this.setState({query: newData, result: newData}))
+//         .catch(error => alert(error))
+//         }
 
  handleInputChange = e => {
-
-    getStrainOfStrains(e.target.value);
    this.setState({
-     query: this.search.value
+    query: e.target.value,
    })
+
+    getStrainOfStrains(e.target.value).then((response) => {
+        console.log("=== ", response.data);
+        this.setState({
+            result: response.data
+          })
+    });
  }
 
  render() {
    return (
+    <div>
      <form>
        <input
          placeholder="Search for..."
@@ -25,8 +49,17 @@ class Search extends Component {
        />
        <p>{this.state.query}</p>
      </form>
+     {this.state.result.map(item => (
+     <div>
+        <div>{item.name}</div>
+        <div>{item.race}</div>
+        <div>{item.desc}</div>
+     </div>
+    ))}
+     </div>
    )
  }
 }
 
-export default Search
+export default Search;
+
--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
 export function getStrainOfStrains(query) {
-  console.log('Query:', query);
-   axios.get(`http://strainapi.evanbusse.com/yh2fbKy/strains/search/name/${query}`)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  //console.log('Query:', query);
+  return axios.get(`http://strainapi.evanbusse.com/yh2fbKy/strains/search/name/${query}`)
+  
 };
